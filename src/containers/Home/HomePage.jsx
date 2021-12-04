255551f3c640411ceef552738b4dadf103cf4ae5
Nikita Kovalenko <nikkv.ottf@gmail.com>
2021-12-04 13:09:51 +0200

    added custom input and popup

--- src/containers/Home/HomePage.jsx
+++ src/containers/Home/HomePage.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import { FaPlus } from 'react-icons/fa';
+import Select from 'react-select';
+import Button from '../../components/Button/Button';
 import CalendarComponent from '../../components/CalendarComponent/CalendarComponent';
+import { Input } from '../../components/Input/Input';
+import { Popup } from '../../components/Popup/Popup';
 import axiosGQLInstance from '../../global/js/axiosGQLInstance';
 import graphql from '../../global/js/graphql';
-import Layout from '../../Layout/Layout';
 import Tasks from '../Tasks/Tasks';
 import Style from './HomePage.module.scss';
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
             tasks: [],
             errors: [],
+            isTaskAdding: false,
+            newTaskName: '',
         };
+
+        this.difficultyOptions = [
+            { value: 0, label: 'Easier' },
+            { value: 1, label: 'Easy' },
+            { value: 2, label: 'Normal' },
+            { value: 3, label: 'Hard' },
+            { value: 4, label: 'Harder' },
+        ];
     }
 
     componentDidMount() {
@@ -34,14 +49,56 @@ export default class HomePage extends Component {
             });
     }
 
+    handleAddButtonClick = () => {
+        this.setState({
+            isTaskAdding: true,
+        });
+    };
+
+    handleAddTaskInputChange = e => {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name]: value,
+        });
+    };
+
     render() {
-        const { tasks, errors } = this.state;
+        const { tasks, errors, isTaskAdding, newTaskName } = this.state;
+
+        const popup = isTaskAdding ? (
+            <Popup onDismiss={() => this.setState({ isTaskAdding: false })}>
+                <Input
+                    label="Enter a task name"
+                    value={newTaskName}
+                    type="text"
+                    id="newTaskName"
+                    name="newTaskName"
+                    onChange={this.handleAddTaskInputChange}
+                />
+                <Select
+                    options={this.difficultyOptions}
+                    defaultValue={this.difficultyOptions[0]}
+                />
+                <div className={Style.PopupButtonWrapper}>
+                    <Button>Submit</Button>
+                </div>
+            </Popup>
+        ) : null;
 
         return (
-            <div className={Style.TasksCalendarWrapper}>
-                <Tasks tasks={tasks} className={Style.Tasks} />
-                <CalendarComponent className={Style.Calendar} />
-            </div>
+            <>
+                <div className={Style.AddTaskWrapper}>
+                    <Button onClick={this.handleAddButtonClick}>
+                        <FaPlus />
+                    </Button>
+                </div>
+                <div className={Style.TasksCalendarWrapper}>
+                    <Tasks tasks={tasks} className={Style.Tasks} />
+                    <CalendarComponent className={Style.Calendar} />
+                </div>
+                {popup}
+            </>
         );
     }
 }
