be39330aa89f42626aab0f2d6e2d148a0b6ec1f4
Max <rendon4ik@gmail.com>
2021-12-04 20:57:16 +0200

    Added login form

--- src/components/AuthBox/AuthBox.jsx
+++ src/components/AuthBox/AuthBox.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Button from '../Button/Button';
 import Style from './AuthBox.module.scss';
 
 const AuthBox = props => {
     const { children, onClick } = props;
 
-    return (
-        <div className={Style.Wrapper}>
-            {children}
-            <Button onClick={onClick} />
-        </div>
-    );
+    return <div className={Style.Wrapper}>{children}</div>;
 };
 
 export default AuthBox;
