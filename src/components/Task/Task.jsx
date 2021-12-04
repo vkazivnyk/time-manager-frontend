655cf386e0bb056937e961f770fbde03adbe0b02
Max <rendon4ik@gmail.com>
2021-12-04 02:29:49 +0200

    Added fields for date in Task component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -12,13 +12,21 @@ const selectBorderColor = difficulty => {
 const Todo = props => {
     const { children, onClickEdit, onClickDelete } = props;
     const difficulty = 0;
+    const startDate = 'startDate';
+    const endDate = 'endDate';
     selectBorderColor(difficulty);
 
     return (
         <div
             className={Style.Wrapper}
             style={{ borderColor: `${borderColor}` }}>
-            <div className={Style.TaskWrapper}>{children}</div>
+            <div className={Style.TaskWrapper}>
+                {children}
+                <div className={Style.DateWrapper}>
+                    <div className={Style.StartDateWrapper}>{startDate}</div>
+                    <div className={Style.EndDateWrapper}>{endDate}</div>
+                </div>
+            </div>
             <button type="button" onClick={onClickEdit}>
                 &#9998;
             </button>
